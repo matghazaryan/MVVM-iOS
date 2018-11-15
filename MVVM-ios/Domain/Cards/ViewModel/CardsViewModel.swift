947a8f098d84fbc92d051d4cdb6b9ff99ff44aa3
Hovhannes Stepanyan <hovhannesstepanyan@Hovhanness-Mac-mini.local>
2018-11-15 09:53:53 +0400

    add new files

---
+++ MVVM-ios/Domain/Cards/ViewModel/CardsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CardsViewModel.swift
+//  MVVM-ios
+//
+//  Created by Hovhannes Stepanyan on 11/15/18.
+//  Copyright © 2018 Matevos Ghazaryan. All rights reserved.
+//
+
+import Foundation
---
+++ MVVM-ios/Domain/Transactions/View/TransactionCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionCell.swift
+//  MVVM-ios
+//
+//  Created by Hovhannes Stepanyan on 11/15/18.
+//  Copyright © 2018 Matevos Ghazaryan. All rights reserved.
+//
+
+import UIKit
+
+class TransactionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
