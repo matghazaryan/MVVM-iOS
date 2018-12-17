fa77987ef4a03730410f2ae59c4968ec8a9fc06a
Hovhannes Stepanyan <hovhannes@helix.am>
2018-12-17 10:33:58 +0400

    correct remove all arranged subviews form stackview

--- MVVM-ios/General/Extension/UIStackView+Extensions.swift
+++ MVVM-ios/General/Extension/UIStackView+Extensions.swift
@@ -10,8 +10,15 @@ import UIKit
 
 extension UIStackView {
     func removeAllArrangedSubviews() {
-        for subView in self.arrangedSubviews {
-            self.removeArrangedSubview(subView)
+        let removedSubviews = self.arrangedSubviews.reduce([]) { allSubviews, subview -> [UIView] in
+            self.removeArrangedSubview(subview)
+            return allSubviews + [subview]
         }
+        
+        // Deactive all constraints
+        NSLayoutConstraint.deactivate(removedSubviews.flatMap({ $0.constraints }))
+        
+        // Remove the views from self
+        removedSubviews.forEach({ $0.removeFromSuperview() })
     }
 }
